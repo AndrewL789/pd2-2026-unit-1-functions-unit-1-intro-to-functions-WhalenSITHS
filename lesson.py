326a2a9a041f0327fac4b45d9b6a2feb908c5759
AndrewL789 <Andrewl434@nycstudents.net>
2025-09-09 09:19:55 -0400

    done?

---
+++ lesson.py
@@ -0,0 +1,26 @@
+""" #def is used to define a function name the funtion parenthesis are parameters and inputs 
+def add(x,y):
+    print(x+y)
+add (5, 15)
+add (5, 25) 
+ """
+""" import turtle
+from turtle import *
+t=Turtle()
+t.forward(x)
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+square(200)
+square(100)  """
+
+
+
+for i in range(3):
+    print(i)
+    
--- test.py
+++ test.py
@@ -1,1 +1,36 @@
-print("hello")
+import turtle
+from turtle import *
+t=Turtle()
+
+
+t.shape('turtle')
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+square(200)
+
+def equal(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    
+equal(200)
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+    
+right()
+
+
